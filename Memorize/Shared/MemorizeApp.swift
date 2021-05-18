34c404d6479d843c17b77ed880916d5490edb313
Pao Yu <51064667+pyxn@users.noreply.github.com>
2021-05-17 21:42:19 -0400

    start empty project

---
+++ Memorize/Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Pao Yu on 2021-05-17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
